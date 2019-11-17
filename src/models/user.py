bf6223bd776a2636b48f9b70360b4dbec22d29f0
iCurbix <adasiek220@gmail.com>
2019-11-17 21:23:29 +0100

    add user model

---
+++ src/models/user.py
@@ -0,0 +1,27 @@
+from src.db import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.token = None
+
+    @classmethod
+    def get_by_username(cls, username):
+        return cls.query.filter_by(username=username).First()
+
+    def add_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def set_token(self, token):
+        self.token = token
